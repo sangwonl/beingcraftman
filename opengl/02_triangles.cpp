c94aac28f436ac3a656786f44aa5477b07c92fdf
gamz <gamzabaw@gmail.com>
2026-01-16 11:46:49 +0900

    update opengl project for mac compatibility

--- opengl/02_triangles.cpp
+++ opengl/02_triangles.cpp
@@ -36,6 +36,7 @@ int main() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
 #ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
