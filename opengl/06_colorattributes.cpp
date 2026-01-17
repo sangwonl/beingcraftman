ae6fa02a984a28e0fd93dfbc43d8666193e2eaf4
gamz <gamzabaw@gmail.com>
2026-01-17 09:29:57 +0900

    opengl: color attribute example

---
+++ opengl/06_colorattributes.cpp
@@ -0,0 +1,152 @@
+#define GLFW_INCLUDE_NONE
+
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+#include <iostream>
+
+#include "module/Shader.hpp"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main() {
+  // glfw: initialize and configure
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+  // glfw window creation
+  GLFWwindow* window =
+      glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // glad: load all OpenGL function pointers
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  // build and compile our shader program
+  Shader ourShader("shaders/shader-attribute-color.vs",
+                   "shaders/shader-attribute-color.fs");
+
+  // set up vertex data (and buffer(s)) and configure vertex attributes
+  float vertices[] = {
+      0.5f,  0.5f,  0.0f, 1.0f, 1.0f, 1.0f,  // top right (white)
+      0.5f,  -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom right (red)
+      -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom left (green)
+      -0.5f, 0.5f,  0.0f, 0.0f, 0.0f, 1.0f,  // top left (blue)
+  };
+
+  unsigned int indices[] = {
+      // note that we start from 0!
+      // 0, 1, 3,  // first triangle
+      1, 2, 3  // second triangle
+  };
+
+  unsigned int VBO, VAO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+  // bind the Vertex Array Object first, then bind and set vertex buffer(s),
+  // and then configure vertex attributes(s).
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                        (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  // note that this is allowd, the call to glVertexAttribPointer registered VBO
+  // as the vertex attribute's bound vertex buffer object
+  // so afterwards we can safely unbind
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // remember: do NOT unbind the EBO while a VAO is active
+  // as the bound element buffer object IS stored in the VAO;
+  // keep the EBO bound.
+  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+  // You can unbind the VAO afterwards so other VAO calls won't accidentally
+  // modify this VAO, but this rarely happens. Modifying other VAOs requries a
+  // call to glBindVertexArray always so we generally don't unbind VAOs (nor
+  // VBOs) when it's not directly necessary.
+  glBindVertexArray(0);
+
+  // uncomment  this call to draw in wireframe polygons;
+  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+  // render loop
+  while (!glfwWindowShouldClose(window)) {
+    // input
+    processInput(window);
+
+    // render
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // draw our first triangle
+    ourShader.use();
+
+    glBindVertexArray(VAO);
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+    // glBindVertexArray(0); // no need to unbind it every time
+
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
+    // etc.)
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // optional: de-allocate all resources once they've outlived their purpose.
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
+
+  // glfw: terminate, clearing all previously allocated GLFW resources.
+  glfwTerminate();
+
+  return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released
+// this frame and react accordingly
+void processInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback
+// function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  // make sure the viewport matches the new window dimensions; note that idth
+  // and height will be significantly larger than specified on retina displays.
+  glViewport(0, 0, width, height);
+}
---
+++ opengl/shaders/shader-attribute-color.vs
@@ -0,0 +1,11 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+
+out vec4 ourColor;
+
+void main()
+{
+    gl_Position = vec4(aPos.xyz, 1.0f);
+    ourColor = vec4(aColor, 1.0f);
+};
